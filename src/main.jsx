069baed2179091c549ee57dc7fad0fd92b6ee9f9
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2024-07-08 11:27:56 +0330

    Added TailwindCSS & DaisyUI...

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./components/app.component"
+import "./assets/main.css"
 
 const container = document.querySelector('#root')
 const root = ReactDOM.createRoot(container)
