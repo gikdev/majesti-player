bde2b56e69d18b35b5d9583db6e8c674f1fce8b8
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2024-07-09 01:00:02 +0330

    Changed Tailwind config & removed **daisyUI** temp...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/*.{js,jsx,ts,jsx}",
-    "./src/**/*.{js,jsx,ts,jsx}",
-    "./src/**/**/*.{js,jsx,ts,jsx}",
-  ],
-  plugins: [require("daisyui")],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
 }
