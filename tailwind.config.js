ba9dbbb33f523522b9727bf5570286a94462ceea
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2024-07-09 01:20:11 +0330

    Added 2 themes!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      textColor: {
+        skin: {
+          base: "var(--clr-text-base)",
+          muted: "var(--clr-text-muted)",
+        },
+      },
+      backgroundColor: {
+        skin: {
+          base: "var(--clr-bg-base)",
+          muted: "var(--clr-bg-muted)",
+        },
+      },
+      colors: {
+        skin: {
+          primary: "var(--clr-primary)",
+        },
+      },
+    },
+  },
 }
